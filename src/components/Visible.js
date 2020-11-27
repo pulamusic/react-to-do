de8873835cd8c091a282753dfa7873fba7445c21
jim@pulamusic.com <jim@pulamusic.com>
2020-11-26 20:53:59 -0500

    workable state I will merge to master

---
+++ src/components/Visible.js
@@ -0,0 +1,18 @@
+import { Component } from 'react'
+import bodyImg from '../assets/sunRa.jpg'
+
+class Visible extends Component {
+  hideDiv = () => {
+    /* When */
+  }
+  render() {
+    return (
+      <figure className="App-main-figure">
+        <img src={bodyImg} alt="Sun Ra again" className="App-main-body-img-2" />
+        <figcaption>hiddenRa</figcaption>
+      </figure>
+    )
+  }
+}
+
+export default Visible
