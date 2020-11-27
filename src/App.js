3934058201fc935fc1a6bed0354eba18dd91c8cc
jim@pulamusic.com <jim@pulamusic.com>
2020-11-27 08:55:25 -0500

    cleaning up before feature branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,29 @@ import Nav from './components/Nav'
 import Todos from './components/Todos'
 
 class App extends Component {
+
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'buy an eraser',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'don\'t be an idiot',
+        completed: false
+      }
+    ]
+  }
+
   render() {
+    console.log(this.state.todos)
     return (
       <div className="container">
         <Nav />
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,16 +2,29 @@ import React, { Component } from 'react'
 import logo from '../assets/adinkra-sm.png'
 
 class Nav extends Component {
+
+  expandNav = () => {
+    const x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
   render() {
     return (
       <nav className="App-nav" id="navbar">
         <div className="App-nav-logo">
           <a href="/"><img src={logo} alt="Logo"/></a>
         </div>
-        <ul>
+        <ul id="navList">
           <li><a href="/">home</a></li>
           <li><a href="#main">main content</a></li>
         </ul>
+        <a href="javascript:void(0);" className="icon" onclick="expandNav()">
+          <i class="fa fa-bars"></i>
+        </a>
       </nav>
     )
   }
